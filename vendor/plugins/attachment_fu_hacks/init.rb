8a0810ea0180b975eb795261e25182d2ce36e188
Chris Scharf <scharfie@gmail.com>
2008-02-26 10:37:18 -0500

    Added attachment_fu hacks to fix attachment_fu with edge Rails' new callback mechanism

---
+++ vendor/plugins/attachment_fu_hacks/init.rb
@@ -0,0 +1,27 @@
+# Hacked to fix attachment_fu with edge Rails
+klass = Technoweenie::AttachmentFu::InstanceMethods
+klass.module_eval do
+  def self.included( base )
+    base.define_callbacks *[:after_resize, :after_attachment_saved, :before_thumbnail_saved]
+  end  
+
+  def callback_with_args(method, arg = self)
+    notify(method)
+    
+    options = { :object => arg }
+    options[:object] = [self, arg] if method == :before_thumbnail_saved
+    
+    result = run_callbacks(method, options) { |result, object| result == false }
+
+    if result != false && respond_to_without_attributes?(method)
+      result = send(method)
+    end
+
+    return result
+  end      
+
+  def run_callbacks(kind, options = {}, &block)
+    options.reverse_merge!( :object => self, :parent => self )
+    ::ActiveSupport::Callbacks::Callback.run(self.class.send("#{kind}_callback_chain"), options[:object], options, &block)
+  end      
+end
