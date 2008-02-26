8f43a25ef6750a36ffc6055800c8ef105abd45c2
Chris Scharf <scharfie@gmail.com>
2008-02-26 12:14:32 -0500

    Removed unnecessary routes, version from footer

--- config/routes.rb
+++ config/routes.rb
@@ -34,12 +34,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => 'account', 
     :member => { :login => :any, :logout => :get }
   
-  # custom routes for verify and approve because they
-  # require certain parameters
   map.with_options :controller => 'account' do |m|
-    m.verify_account 'account/verify/:id/:key', :action => 'verify'
-    m.approve_account 'account/approve/:id/:key', :action => 'approve'
-    
     m.login 'login', :action => 'login', :erp => '/account/login'
     m.logout 'logout', :action => 'logout', :erp => '/account/logout'
   end
