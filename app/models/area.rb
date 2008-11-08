24131100bb27c5fad415288d001f24a6481d3755
Chris Scharf <scharfie@gmail.com>
2008-11-08 09:15:43 -0500

    Refactored area versioning into a plugin

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,34 +1,24 @@
 class Area < ActiveRecord::Base
+  # Acts
+  acts_as_published
   compiled_column :body
+
+  # Associations
   belongs_to :page
   belongs_to :user
 
-  # Version 0 is draft, version 1 is published, 
-  # versions 2-5 are previously published 
-  has_many :versions, :class_name => 'Area', 
-    :foreign_key => 'area_id', :dependent => :delete_all
-  
+  # Callbacks
+  before_validation :ensure_user
+
+  # Validations
   validates_presence_of :page_id
   validates_presence_of :user_id
   validates_presence_of :key
-
-  before_validation :ensure_user
 
 protected  
   # Ensures that user is set
   def ensure_user  
     self.user = User.active
-  end
-  
-  # Increments all versions of this area
-  def increment_versions
-    self.class.update_all 'version = version + 1', 
-      "area_id = #{self.id} AND version > 0"
-  end
-
-  # Removes old versions (any version >= 5) of this area
-  def clear_old_versions
-    self.class.delete_all "version >= 5 AND area_id = #{self.id}"
   end
     
 public
@@ -39,25 +29,6 @@ public
       'WHERE a.key = ? AND (p.space_id = ? AND a.is_default = ?) ' +
       "ORDER BY a.is_default ASC", key, space_id, true]).first
   end
-
-  # Publishes this area
-  def publish!
-    return false if new_record?
-    
-    returning self.clone do |published_area|
-      transaction do
-        increment_versions 
-        clear_old_versions
-        published_area.update_attributes(
-          :area_id => self.id, :version => 1, :published_on => Time.now)
-      end
-    end  
-  end
-
-  # Finds currently published version
-  def published_version
-    versions.find_by_version(1)
-  end 
 
   # Returns key for URL parameter
   def to_param
---
+++ vendor/plugins/acts_as_published/lib/active_record/acts/published.rb
@@ -0,0 +1,82 @@
+module ActiveRecord #:nodoc:
+  module Acts #:nodoc:
+    module Published
+      def self.included(base)
+        base.extend ClassMethods
+        base.send :include, InstanceMethods  
+      end
+    
+      module ClassMethods
+        # Version 0 is draft, version 1 is published, 
+        # versions 2-5 are previously published 
+        def acts_as_published(options={})
+          options.reverse_merge! acts_as_published_default_configuration(self.name)
+          
+          # Define a method to access the provided configuration
+          class_eval "def self.acts_as_published_configuration(); #{options.inspect}; end"
+
+          # Add the association
+          has_many :versions, options
+        end
+      
+        # Returns hash of default options based on given
+        # class name
+        def acts_as_published_default_configuration(class_name)
+          options = {
+            :class_name  => class_name,
+            :foreign_key => class_name.foreign_key,
+            :dependent   => :delete_all
+          }  
+        end
+      end
+    
+      module InstanceMethods
+        # Accessor to class method
+        def acts_as_published_configuration
+          self.class.acts_as_published_configuration
+        end
+        
+        # Increments all versions of this model
+        def increment_versions
+          self.class.update_all 'version = version + 1', 
+            "#{acts_as_published_configuration[:foreign_key]} = #{self.id} AND version > 0"
+        end
+
+        # Removes old versions (any version >= 5) of this model
+        def clear_old_versions
+          self.class.delete_all "version >= 5 AND #{acts_as_published_configuration[:foreign_key]} = #{self.id}"
+        end  
+        
+        # Publishes this model
+        def publish!
+          return false if new_record?
+    
+          returning self.clone do |published_model|
+            transaction do
+              increment_versions 
+              clear_old_versions
+              published_model.update_attributes(
+                acts_as_published_configuration[:foreign_key] => self.id, 
+                :version => 1, 
+                :published_on => Time.now
+              )
+            end
+          end  
+        end
+
+        # Finds currently published version
+        def published_version
+          versions.find_by_version(1)
+        end
+        
+        def published?
+          version == 1
+        end
+        
+        def draft?
+          version == 0
+        end
+      end
+    end
+  end
+end
