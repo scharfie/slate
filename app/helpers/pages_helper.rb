c573e9c0ebe73a8ec0bd92eff152f3c63f933367
Chris Scharf <scharfie@gmail.com>
2009-02-07 08:43:36 -0500

    Mount pages have different icon

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,6 +2,7 @@ module PagesHelper
   # Returns glyph for given page
   def page_glyph(page)
     return glyph('house') if page.default?
+    return glyph('cog') if page.mount?
     glyph('page_white')
   end
 end
